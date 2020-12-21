7e8de26e8f019c685db5bd097cd24b5b1293607f
StanSauce <stan.williams@saucelabs.com>
2020-12-21 14:11:22 -0600

    Add Output

--- tapestry_dev.sh
+++ tapestry_dev.sh
@@ -8,8 +8,9 @@
 # This shell script will launch safari, visit a basic-auth reserved site, and log in.
 
 
+echo 'Running tapestry executable'
 
 osascript <<-EOF 
 
-tell application "Safari"	open location "https://production.stuartweitzman.com"	activate		tell application "System Events" to tell process "Safari"		--	keystroke tab		keystroke "storefront" & tab & "Stuart123" & return	end tellend tell
+tell application "Safari"	open location "https://development.stuartweitzman.com"	activate		tell application "System Events" to tell process "Safari"		--	keystroke tab		keystroke "storefront" & tab & "Stuart123" & return	end tellend tell
 EOF
