73e9d16b93596969a802080de8650470c01ac0c0
StanSauce <stan.williams@saucelabs.com>
2020-12-21 11:03:38 -0600

    First Commit

---
+++ tapestry_dev.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Tapestry
+# Pre-Run Executable
+# Stan Williams, Sauce Labs
+# December 21, 2020
+
+# This shell script will launch safari, visit a basic-auth reserved site, and log in.
+
+
+
+osascript <<-EOF 
+tell application "Safari"	# open location "https://production.stuartweitzman.com"	activate	delay 1	tell application "System Events" to tell process "Safari"		--	keystroke tab		keystroke "storefront" & tab & "Stuart123" & return	end tellend tell
+EOF
