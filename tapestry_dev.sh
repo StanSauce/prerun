e0441959c35b401bcff92a472495bbe77fee0f62
StanSauce <stan.williams@saucelabs.com>
2020-12-22 13:18:30 -0600

    Delay before new Tab

--- tapestry_dev.sh
+++ tapestry_dev.sh
@@ -18,33 +18,16 @@ delay 5
 tell application "Safari"
 	activate
 
-	tell application "System Events" to tell process "Safari"	 		keystroke "," using command down
-        	set frontmost to true
-        	tell window 1
-            		click button "Advanced" of toolbar 1
-            		delay 2
-            		set theCheckbox to checkbox 4 of group 1 of group 1 of it
-            		tell theCheckbox
-                		if not (its value as boolean) then click theCheckbox
-            		end tell
-            		delay 2
-            		keystroke "w" using command down
-            		delay 2
-        	end tell
-        	tell menu bar item "Develop" of menu bar 1
-            	click
-            	delay 2
-            	click menu item "Allow Remote Automation" of menu 1
-            	delay 2
-        	end tell
-	end tell
+
 
 
     open location "https://development.stuartweitzman.com"	
     delay 2
 	
     tell application "System Events" to tell process "Safari"	
-	-- provide credentials, submit	keystroke "storefront" & tab & "Stuart123" & return    end tell
+	-- provide credentials, submit	keystroke "storefront" & tab & "Stuart123" & return
+	delay 2
+	keystroke "t" using command down    end tell
 	end tell
 EOF
 
