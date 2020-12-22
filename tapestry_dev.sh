2fe4648d54528becc4054d81638ef7cf83ebfda3
StanSauce <stan.williams@saucelabs.com>
2020-12-22 12:31:04 -0600

    Attempt Dev Menu Enablement

--- tapestry_dev.sh
+++ tapestry_dev.sh
@@ -23,7 +23,27 @@ tell application "Safari"
 
 	#delay 2
 
-	tell application "System Events" to tell process "Safari"		--	keystroke tab		keystroke "storefront" & tab & "Stuart123" & return	end tellend tell
+
+	tell application "System Events" to tell process "Safari"	 		keystroke "," using command down
+        	set frontmost to true
+        	tell window 1
+            		click button "Advanced" of toolbar 1
+            		delay 2
+            		set theCheckbox to checkbox 4 of group 1 of group 1 of it
+            		tell theCheckbox
+                		if not (its value as boolean) then click theCheckbox
+            		end tell
+            		delay 2
+            		keystroke "w" using command down
+            		delay 2
+        	end tell
+        	tell menu bar item "Develop" of menu bar 1
+            	click
+            	delay 2
+            	click menu item "Allow Remote Automation" of menu 1
+            	delay 2
+        	end tell		
+	--	keystroke tab		keystroke "storefront" & tab & "Stuart123" & return	end tellend tell
 EOF
 
 echo 'Complete Execution'
