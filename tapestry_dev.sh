dfefe0afe6d2cf7ac8ba0c46f2bb458c9b743391
StanSauce <stan.williams@saucelabs.com>
2020-12-22 12:50:20 -0600

    Open New Tab for Basic-Auth URL Visit

--- tapestry_dev.sh
+++ tapestry_dev.sh
@@ -13,41 +13,20 @@ echo 'Running tapestry executable'
 osascript <<-EOF 
 
 # run after site visited
-# delay 10
+delay 10
 
 tell application "Safari"
 
 	activate
-
-	#open location "https://development.stuartweitzman.com"
-
-	#delay 2
-
-
-	tell application "System Events" to tell process "Safari"	 		keystroke "," using command down
-        	set frontmost to true
-        	tell window 1
-            		click button "Advanced" of toolbar 1
-            		delay 2
-            		set theCheckbox to checkbox 4 of group 1 of group 1 of it
-            		tell theCheckbox
-                		if not (its value as boolean) then click theCheckbox
-            		end tell
-            		delay 2
-            		keystroke "w" using command down
-            		delay 2
-        	end tell
-        	tell menu bar item "Develop" of menu bar 1
-            	click
-            	delay 2
-            	click menu item "Allow Remote Automation" of menu 1
-            	delay 2
-        	end tell
-	end tell	
-	open location "https://development.stuartweitzman.com"
-	delay 2
 	tell application "System Events" to tell process "Safari"	
-	--	keystroke tab		keystroke "storefront" & tab & "Stuart123" & return	end tell
+		-- open a new browser tab		keystroke "t" using command down	end tell
+	
+	open location "https://development.stuartweitzman.com"
+
+	delay 2
+	
+	tell application "System Events" to tell process "Safari"	
+		-- provide credentials, submit		keystroke "storefront" & tab & "Stuart123" & return	end tell
 	end tell
 EOF
 
